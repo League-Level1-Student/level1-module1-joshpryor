293b93d10cd5849bc04a9b7fc9f5782863502390
William Kwak <league@worthymini-5.attlocal.net>
2018-08-14 11:10:12 -0700

    bbbbiinary converter

--- src/binaryConverter.java
+++ src/binaryConverter.java
@@ -1,6 +1,7 @@
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -33,13 +34,40 @@ public class binaryConverter implements ActionListener {
 		
 		
 	window1.pack();
+	window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}
+	
+	  String convert(String input) {
+          if(input.length() != 8){
+               JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+               return "-";
+          }
+          String binary = "[0-1]+";    //must contain numbers in the given range
+          if (!input.matches(binary)) {
+               JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+               return "-";
+          }
+          try {
+               int asciiValue = Integer.parseInt(input, 2);
+               char theLetter = (char) asciiValue;
+               return "" + theLetter;
+          } catch (Exception e) {
+               JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+               return "-";
+          }
+     }
+
+	  
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
 		
 		if (buttonPressed == butt1) {
-			h =  ("A");
+			String input = textthing.getText();
+		String yeet = 	convert(input);
+			h =  (yeet);
 			lable1.setText(h);
+			
+			
 		}
 		
 		
